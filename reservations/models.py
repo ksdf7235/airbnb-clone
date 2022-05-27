22bd6fdd75d9104712e992d71c9ab44007aca6ac
ksdf7235 <85724877+ksdf7235@users.noreply.github.com>
2022-05-27 12:53:37 +0900

    update(reservation)

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,28 @@
+from operator import mod
+from tabnanny import check
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Reservation(core_models.TimeStampedModel):
+
+    STATUS_PENDING = "pending"
+    STATUS_CONFIRMED = "confiremd"
+    STATUS_CANCELED = "canceled"
+
+    STATUS_CHOICE = (
+        (STATUS_PENDING, "Pending"),
+        (STATUS_CONFIRMED, "Confiremd"),
+        (STATUS_CANCELED, "Canceled"),
+    )
+
+    status = models.CharField(
+        max_length=12, choices=STATUS_CHOICE, default=STATUS_PENDING
+    )
+    check_in = models.DateField()
+    check_in = models.DateField()
+    guest = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.room} - {self.check_in}"
