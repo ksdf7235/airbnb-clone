88664b93e11d29f90db19de5969d6b0a0d9484bf
ksdf7235 <85724877+ksdf7235@users.noreply.github.com>
2022-05-27 12:42:47 +0900

    update(reviews)

--- rooms/models.py
+++ rooms/models.py
@@ -1,11 +1,56 @@
 from pydoc import describe
+from tabnanny import verbose
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
 
 
-class Room(core_models.TimeStampedModle):
+class AbstractItem(core_models.TimeStampedModel):
+
+    """Abstract Item"""
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["name"]
+
+
+class Amenity(AbstractItem):
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+    class Meta:
+        verbose_name_plural = "facilities"
+
+
+class HouseRule(AbstractItem):
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
+
+class Room(core_models.TimeStampedModel):
 
     pass
 
@@ -22,4 +67,13 @@ class Room(core_models.TimeStampedModle):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey(
+        "RoomType", blank=True, on_delete=models.SET_NULL, null=True
+    )
+    Amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_ules = models.ManyToManyField("HouseRule", blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
