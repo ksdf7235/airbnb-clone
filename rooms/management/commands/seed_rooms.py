ced38d018139093cee3abf15eec164ce1d7307d7
ksdf7235 <85724877+ksdf7235@users.noreply.github.com>
2022-06-09 17:35:13 +0900

    update(seeds_rooms)

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,6 +1,9 @@
+import random
+from secrets import choice
 from django.core.management.base import BaseCommand
 from django_seed import Seed
-from users.models import User
+from rooms import models as room_models
+from users import models as user_models
 
 
 class Command(BaseCommand):
@@ -9,12 +12,28 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "--number", default=2, type=int, help="How many users you want to create"
+            "--number", default=2, type=int, help="How many rooms you want to create"
         )
 
     def handle(self, *args, **options):
         number = options.get("number")
         seeder = Seed.seeder()
-        seeder.add_entity(User, number, {"is_staff": False, "is_superuser": False})
+        all_users = user_models.User.objects.all()
+        room_types = room_models.RoomType.objects.all()
+        print(all_users, room_types)
+        seeder.add_entity(
+            room_models.Room,
+            number,
+            {
+                "name": lambda x: seeder.faker.address(),
+                "host": lambda x: random.choice(all_users),
+                "room_type": lambda x: random.choice(room_types),
+                "price": lambda x: random.randint(1, 300),
+                "guests": lambda x: random.randint(1, 20),
+                "beds": lambda x: random.randint(1, 5),
+                "bedrooms": lambda x: random.randint(1, 5),
+                "baths": lambda x: random.randint(1, 5),
+            },
+        )
         seeder.execute()
-        self.stdout.write(self.style.SUCCESS(f"{number} users created!"))
+        self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
