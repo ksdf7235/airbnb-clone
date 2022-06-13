cc47283bd196d41774b8eabf998b8439660ac523
ksdf7235 <85724877+ksdf7235@users.noreply.github.com>
2022-06-13 13:57:38 +0900

    model base view

--- rooms/views.py
+++ rooms/views.py
@@ -1,19 +1,20 @@
 from math import ceil
+from django.utils import timezone
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
 
 
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(
-        request,
-        "rooms/home.html",
-        context={"rooms": rooms},
-    )
+class HomeView(ListView):
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
