4ee36110449eabc05f373df08ad83952e14c09fb
ksdf7235 <85724877+ksdf7235@users.noreply.github.com>
2022-06-15 10:16:53 +0900

    update(search)

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 # Create your views here.
@@ -13,3 +14,9 @@ class HomeView(ListView):
 
 class RoomDetail(DetailView):
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
